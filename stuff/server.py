72d8c2b93c287e7a23066c1de85af4758678535e
wisedrette <ny4koo@gmail.com>
2017-10-28 00:48:13 +0200

    test commit

--- stuff/server.py
+++ stuff/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<body>Hello world. <a href="/about">About this page</a>.</body>'
+    return '<body>Hello world. <a href="/about/">About this page</a>.</body>'
 
 @app.route('/about')
 def about():
