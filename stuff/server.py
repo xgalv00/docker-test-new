5ce8ca67bc799ddfa08d217f839cfdb1ccebee52
wisedrette <ny4koo@gmail.com>
2018-06-04 21:42:34 +0200

    message spamming logic

--- stuff/server.py
+++ stuff/server.py
@@ -1,14 +1,31 @@
 from flask import Flask
+import threading
+import time
+import logging
 
 app = Flask(__name__)
+
+
+@app.before_first_request
+def activate_job():
+    def run_job():
+        while True:
+            logging.warning("Run recurring task")
+            time.sleep(0.25)
+
+    thread = threading.Thread(target=run_job)
+    thread.start()
+
 
 @app.route('/')
 def index():
     return '<body>Hello world. <a href="/about/">About this page</a>.</body>'
 
+
 @app.route('/about')
 def about():
     return '<body>This is the about page</body>'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
